c7901faeb47736d32141513bd3febe0d22493c10
vibha <vibha.p@elaunchinfotech.in>
2023-07-23 20:20:54 +0530

    fetched weather data city wise

--- app/src/main/java/com/example/androidassignemnt2/api/WeatherReportClient.kt
+++ app/src/main/java/com/example/androidassignemnt2/api/WeatherReportClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object WeatherReportClient {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "1fbceea75862b6a0ac0ee61a8219b370"
+    const val API_KEY = "cb1659e0cd6187347cdd0b4dc7e75320"
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
